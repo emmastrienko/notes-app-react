4b70910d012cfea69011b509a2afd4cdd1849109
Emma Strienko <mstrienko@gmail.com>
2024-09-27 19:43:29 +0300

    sign up component

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
     setError("")
 
     // Login API Call
-    
+
   }
 
   return (
@@ -58,7 +58,7 @@ const Login = () => {
 
             <p className="text-sm text-center mt-4 ">
               Not registered yet? {""}
-              <Link to="signUp" className="font-medium text-primary underline">
+              <Link to="/signUp" className="font-medium text-primary underline">
                 Create an Account
               </Link>
             </p>
