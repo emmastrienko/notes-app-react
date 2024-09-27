8b6098d9a8291c8a5476c3f509645c0af8238242
Emma Strienko <mstrienko@gmail.com>
2024-09-27 19:18:56 +0300

    login component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2bb5ff",
+        secondary: "#ef863e"
+      }
+    },
   },
   plugins: [],
 }
