db9afd74a94762eb2b8991af732d24a2b61e273f
Emma Strienko <mstrienko@gmail.com>
2024-09-28 16:02:44 +0300

    searchBar and Cards

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,25 @@
+import NoteCard from "../../components/Cards/NoteCard";
+import Navbar from "../../components/Navbar/Navbar";
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Navbar />
+
+      <div className="container mx-auto">
+        <NoteCard
+          title={"Meeting on 7th April"}
+          date={"3rd April 2024"}
+          content={"Meeting on 7th April Meeting on 7th April"}
+          tags={"#meeting"}
+          isPinned={true}
+          onEdit={() => {}}
+          onDelete={() => {}}
+          onPinNote={() => {}}
+        />
+      </div>
+    </>
   );
-}
+};
 
 export default Home;
